1c439801ffc44a7eec6eafb491f11be9ac23efb1
ahmedyasser <ahmedyasser.dev@hotmail.com>
2023-06-06 08:10:33 +0300

    chore(react-ui): removed ref

--- libs/react-ui/src/lib/section/section.tsx
+++ libs/react-ui/src/lib/section/section.tsx
@@ -1,15 +1,9 @@
-import React, { forwardRef, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren } from 'react';
 
 import './section.scss';
 
-const HtmlSectionComponent = forwardRef<HTMLElement, PropsWithChildren>(
-  ({ children }, ref) => {
-    return (
-      <section ref={ref} className="section">
-        {children}
-      </section>
-    );
-  }
-);
+const HtmlSectionComponent: FC<PropsWithChildren> = ({ children }) => {
+  return <section className="section">{children}</section>;
+};
 
 export default HtmlSectionComponent;
