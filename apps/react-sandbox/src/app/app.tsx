b429107cad59c2315d225fdbf2c51076beaeb155
trevorling <53426191+trevorling@users.noreply.github.com>
2023-06-07 10:04:44 +0300

    feat(react-ui): add TableOfContents components

--- apps/react-sandbox/src/app/app.tsx
+++ apps/react-sandbox/src/app/app.tsx
@@ -1,20 +1,71 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ButtonComponent } from '@egov/cvi-react';
+import {
+  ButtonComponent,
+  TableOfContentsComponent,
+  TableOfContentsItemComponent,
+  TableOfContentsWrapperComponent,
+} from '@egov/cvi-react';
 
 import { Link, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <ButtonComponent appearance={ 'primary'} size={'s'}>Click me</ButtonComponent>
-      <div/>
+      <ButtonComponent appearance={'primary'} size={'s'}>Click me</ButtonComponent>
+      <div />
+
+      <TableOfContentsWrapperComponent>
+        <div>
+          <div id="section-one">
+            <h1>Section One</h1>
+            <p>
+              There comes a day when prey kills predator, the young one shall usher
+              forth a strengthening of bonds and new aggressions.
+            </p>
+            <p>
+              This Pokemon is a ice-type Pokemon and looks a lot like an antelope. It
+              has snowy legs, an icicle covered tail and frosty ears. They're
+              generally playful by nature and can often be found in winter. If you're
+              out looking for them they can often be seen lurking about and on their
+              own. It tends to attack with Haze and Ice Punch. It hasn't evolved yet
+              and there are no known evolutions.
+            </p>
+          </div>
+          <div id="section-two">
+            <h1>Section Two</h1>
+            <p>
+              There comes a day when prey kills predator, the young one shall usher
+              forth a strengthening of bonds and new aggressions.
+            </p>
+            <p>
+              This Pokemon is a ice-type Pokemon and looks a lot like an antelope. It
+              has snowy legs, an icicle covered tail and frosty ears. They're
+              generally playful by nature and can often be found in winter. If you're
+              out looking for them they can often be seen lurking about and on their
+              own. It tends to attack with Haze and Ice Punch. It hasn't evolved yet
+              and there are no known evolutions.
+            </p>
+          </div>
+        </div>
+        <TableOfContentsComponent title={'Sisukord'}>
+          <TableOfContentsItemComponent
+            label={'Section One with a very long label that spans many lines'}
+            linkTo={'#section-one'}
+            isSelected={true}
+          />
+          <TableOfContentsItemComponent
+            label={'Section One with a very long label that spans many lines'}
+            linkTo={'#section-two'}
+          />
+        </TableOfContentsComponent>
+      </TableOfContentsWrapperComponent>
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
-      <br/>
-      <hr/>
-      <br/>
+      <br />
+      <hr />
+      <br />
       <div role="navigation">
         <ul>
           <li>
--- libs/react-ui/src/index.ts
+++ libs/react-ui/src/index.ts
@@ -3,3 +3,7 @@ export {
   ButtonAppearance,
   ButtonSize,
 } from './lib/button/button';
+
+export { default as TableOfContentsWrapperComponent } from './lib/table-of-contents/table-of-contents-wrapper/table-of-contents-wrapper';
+export { default as TableOfContentsItemComponent } from './lib/table-of-contents/table-of-contents-item/table-of-contents-item';
+export { default as TableOfContentsComponent } from './lib/table-of-contents/table-of-contents';
