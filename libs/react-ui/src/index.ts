4d3f685dec71bc9bd542956e8d6735269323a3b5
baha-a <bhlshrf@gmail.com>
2023-06-08 01:41:50 +0300

    feat(react-ui): rename toast folder

--- libs/react-ui/src/index.ts
+++ libs/react-ui/src/index.ts
@@ -4,9 +4,7 @@ export {
   ButtonSize,
 } from './lib/button/button';
 
-export {
-  default as Toast
-} from './lib/Toast-rename';
+export { default as Toast } from './lib/Toast-rename';
 
 export * from './lib/context';
 export * from './lib/hooks';
