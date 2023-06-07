141dcce094399b4e0110e084b1271cc5e2faf09b
baha-a <bhlshrf@gmail.com>
2023-06-07 10:46:34 +0300

    feat(react-ui): add react steps

--- apps/react-sandbox/src/app/app.tsx
+++ apps/react-sandbox/src/app/app.tsx
@@ -1,14 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ButtonComponent } from '@egov/cvi-react';
+import { useState } from 'react';
+import { ButtonComponent, Steps } from '@egov/cvi-react';
 
 import { Link, Route, Routes } from 'react-router-dom';
 
 export function App() {
+
+  const [step, setStep] = useState(0)
+
   return (
     <>
       <ButtonComponent appearance={ 'primary'} size={'s'}>Click me</ButtonComponent>
       <div/>
 
+      <Steps
+        title='Steps Example'
+        description='Some Description'
+        steps={[
+          { title: 'Step A', content: 'Step a content' },
+          { title: 'Step B', content: 'Step b content' },
+          { title: 'Step C', content: 'Step c content' },
+          { title: 'Step D', content: 'Step d content' },
+        ]}
+        activeStepIndex={step}
+        select={setStep}
+      />
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
--- libs/react-ui/src/index.ts
+++ libs/react-ui/src/index.ts
@@ -3,3 +3,5 @@ export {
   ButtonAppearance,
   ButtonSize,
 } from './lib/button/button';
+
+export { default as Steps } from './lib/Steps';
