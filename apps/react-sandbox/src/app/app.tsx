8bc18f9cf6d79ac4f009adcf69c208205eec7945
trevorling <53426191+trevorling@users.noreply.github.com>
2023-06-09 10:49:40 +0300

    chore(react-ui): synced main

--- apps/react-sandbox/src/app/app.tsx
+++ apps/react-sandbox/src/app/app.tsx
@@ -1,13 +1,187 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ButtonComponent, RadioButtonGroupComponent } from '@egov/cvi-react';
-
+import {
+  ButtonComponent,
+  BreadcrumbsComponent,
+  TabElement,
+  TabGroupComponent,
+  TrackComponent,
+  IconComponent,
+  LabeledIconComponent,
+  TextareaComponent,
+  FormItem,
+  HtmlSectionComponent,
+  ContentContainerComponent,
+  ContentPanelComponent,
+  ScreenreaderTextComponent,
+  SelectComponent,
+  StatusBadgeComponent,
+  AccordionComponent,
+  InputComponent,
+  RadioButtonGroupComponent,
+  ReorderableListComponent,
+} from '@egov/cvi-react';
 import { Link, Route, Routes } from 'react-router-dom';
 
 export function App() {
   return (
     <>
-      <ButtonComponent appearance={'primary'} size={'s'}>Click me</ButtonComponent>
+      <ButtonComponent appearance={'primary'} size={'s'}>
+        Click me
+      </ButtonComponent>
       <div />
+      <SelectComponent
+        style={{ width: 250 }}
+        label={'Some label'}
+        placeholder={'Otsi elementi või lisa uss 🐍'}
+        options={[
+          {
+            label: 'Item 1 that is not that short but actually quite long',
+            value: 'first',
+          },
+          { label: 'item 2', value: 'second' },
+          { label: 'Item that can be selected from keyboard', value: 'third' },
+          { label: 'Item 4', value: 'fourth' },
+          { label: 'Item 5', value: 'fifth' },
+          {
+            label: 'Item 6 with a very long text spanning many lines',
+            value: 'sixth',
+          },
+        ]}
+      ></SelectComponent>
+      <TabGroupComponent>
+        <TabElement title="Tab 1">First tab content</TabElement>
+        <TabElement title="Tab 2">Second tab content</TabElement>
+      </TabGroupComponent>
+
+      <HtmlSectionComponent>
+        <label>CVI </label>
+        <label>Testing </label>
+        <label>Section </label>
+        <HtmlSectionComponent>
+          <label>CVI </label>
+          <label>Testing </label>
+          <label>Section </label>
+        </HtmlSectionComponent>
+      </HtmlSectionComponent>
+
+      <TrackComponent layout="grid" isMultiline={true} justify="center">
+        <ButtonComponent>Button 1</ButtonComponent>
+        <ButtonComponent>Button 2</ButtonComponent>
+      </TrackComponent>
+
+      <div>
+        <TextareaComponent
+          placeholder="Enter Your Name"
+          maxLength={30}
+          showMaxLength={false}
+          resizable={true}
+        ></TextareaComponent>
+      </div>
+
+      <FormItem
+        label={'Your email'}
+        placeholder="placeholder..."
+        labelPosition="side"
+        isLabelHidden={false}
+        required={false}
+        htmlId="id"
+      ></FormItem>
+
+      <IconComponent
+        icon={undefined}
+        label={'close'}
+        size="medium"
+      ></IconComponent>
+
+      <LabeledIconComponent
+        icon={undefined}
+        label={'close'}
+        size="medium"
+        iconPosition="before"
+        iconLabel={'Test Label'}
+        gap={10}
+        insideButton={true}
+      ></LabeledIconComponent>
+
+      <ContentContainerComponent>
+        Create recall build border flat fourth wild however swam motor shake fly
+        hidden running dead bear walk floor plan chance yourself shade hunt
+        came. Lay fence wave several short birth desk thrown clearly official
+        blanket strike neck can card cabin quiet horn leather rocket forth
+        current kill mile. Color paper sad bread substance fact consider soon
+        after west brass impossible perhaps reason interior hit program start
+        section unless peace whatever screen record. Scientific torn spirit
+        period surrounded brief drew account show tube canal meet walk
+        expression just syllable exactly cannot industrial dawn universe worth
+        include movie. Fight game touch ants kept strange win impossible happen
+        deeply company yesterday above different birth nation fall thee pleasant
+        given main wrote appearance situation. Basis box factory bar quickly
+        shut whistle breathe sweet limited while indicate gulf labor stream plan
+        southern green produce break for place concerned discussion.
+      </ContentContainerComponent>
+
+      <ContentPanelComponent title={'CVI'}>
+        Create recall build border flat fourth wild however swam motor shake fly
+        hidden running dead bear walk floor plan chance yourself shade hunt
+        came. Lay fence wave several short birth desk thrown clearly official
+        blanket strike neck can card cabin quiet horn leather rocket forth
+        current kill mile. Color paper sad bread substance fact consider soon
+        after west brass impossible perhaps reason interior hit program start
+        section unless peace whatever screen record. Scientific torn spirit
+        period surrounded brief drew account show tube canal meet walk
+        expression just syllable exactly cannot industrial dawn universe worth
+        include movie. Fight game touch ants kept strange win impossible happen
+        deeply company yesterday above different birth nation fall thee pleasant
+        given main wrote appearance situation. Basis box factory bar quickly
+        shut whistle breathe sweet limited while indicate gulf labor stream plan
+        southern green produce break for place concerned discussion.
+      </ContentPanelComponent>
+
+      <ScreenreaderTextComponent label="Some text that should only be visible to a screen reader."></ScreenreaderTextComponent>
+
+      <StatusBadgeComponent
+        content="waiting"
+        severityStyle="info"
+      ></StatusBadgeComponent>
+
+      <AccordionComponent
+        singleOpen={true}
+        items={[
+          {
+            title: 'Super Simple Accordion',
+            body: 'Some content here.',
+          },
+          {
+            title:
+              'Yet another item, but this time disabled with a very long text',
+            body: 'Disabled Content',
+            disabled: true,
+          },
+        ]}
+      ></AccordionComponent>
+
+      <div>
+        <InputComponent
+          label="Your Name"
+          placeholder="Name..."
+          showMaxLength={false}
+          maxLength={40}
+          disabled={false}
+          icon={undefined}
+        ></InputComponent>
+      </div>
+
+      <ReorderableListComponent
+        items={[
+          { index: 0, value: 'Me as well' },
+          { index: 1, value: 'And so am I' },
+          { index: 2, value: 'I am a reorderable item' },
+        ]}
+      ></ReorderableListComponent>
+
+      <BreadcrumbsComponent
+        labels={['Avaleht', 'Abiellumine', 'Avalduse esitamine']}
+      />
 
       <RadioButtonGroupComponent
         label={'Default radio buttons example'}
@@ -38,7 +212,6 @@ export function App() {
         ]}
         onChange={(selectedValue) => console.log(selectedValue)}
       />
-
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
       {/* Feel free to move and update them to fit your needs */}
