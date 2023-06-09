fd57970b90d19a87c7d9de5ca47a813812df0895
baha-a <bhlshrf@gmail.com>
2023-06-09 13:10:35 +0300

    chore(react-ui): synced main

--- libs/react-ui/src/lib/steps/index.tsx
+++ libs/react-ui/src/lib/steps/index.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react';
 import { Step } from './type';
 import Header from './header';
-import Footer from './footer';
 import './Steps.scss';
+import Footer from './footer';
 
 interface StepsProps {
   title?: string;
   description?: string;
   steps: Step[];
-  activeStepIndex: number;
+  activeStepIndex?: number;
   select: (index: number) => void;
 }
 
@@ -18,35 +18,36 @@ const Steps: FC<StepsProps> = ({
   steps,
   activeStepIndex,
   select,
-}) => {
-  return (
-    <div className="cvi-steps">
+}) => (
+  <div className="cvi-steps">
+    {
+      title &&
+      <h1 className="cvi-steps__title">
+        {title}
+      </h1>
+    }
+    <div className="cvi-steps__inner-wrapper">
       {
-        title &&
-        <h1 className="cvi-steps__title">
-          {title}
-        </h1>
+        description &&
+        <div className="cvi-steps__intro">
+          {description}
+        </div>
       }
-      <div className="cvi-steps__inner-wrapper">
-        {
-          description &&
-          <div className="cvi-steps__intro">
-            {description}
-          </div>
-        }
-        <div className="cvi-steps__inner">
-          <Header steps={steps} activeStepIndex={activeStepIndex} select={select} />
-          <div className='cvi-steps__step'>
+      <div className="cvi-steps__inner">
+        <Header steps={steps} activeStepIndex={activeStepIndex} select={select} />
+        <div className='cvi-steps__step'>
+          {
+            activeStepIndex !== undefined && activeStepIndex !== null &&
             <div className='cvi-steps__content-panel'>
               <h2 className='cvi-steps__content-panel-title'>{steps[activeStepIndex].title}</h2>
               {steps[activeStepIndex].content}
             </div>
-            <Footer steps={steps} activeStepIndex={activeStepIndex} select={select} />
-          </div>
+          }
+          <Footer steps={steps} activeStepIndex={activeStepIndex} select={select} />
         </div>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Steps;
